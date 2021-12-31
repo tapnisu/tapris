8667e51fba58c645784a788d8259cfb515220c36
Alexiy Rybin <alexiy.rybin@gmail.com>
2021-12-31 12:01:53 +0700

    Interaction on /start fixed

--- src/Commands/Music/start.ts
+++ src/Commands/Music/start.ts
@@ -5,6 +5,8 @@ export const command: Command = {
 	name: 'start',
 	description: 'Play music from the queue',
 	run: async (client, interaction) => {
+		interaction.reply('Starting...')
+
 		return play(client, interaction)
 	}
 }
