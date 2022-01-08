812d0c67a2467f730cd99d84f2663c291c268357
Alexiy Rybin <alexiy.rybin@gmail.com>
2022-01-08 11:38:38 +0700

    /user updated

--- src/Commands/Info/user.ts
+++ src/Commands/Info/user.ts
@@ -9,26 +9,21 @@ export const command: Command = {
 			name: 'user',
 			description: 'User to be shown',
 			type: 6,
-			required: false
+			required: true
 		}
 	],
 	run: async (client, interaction) => {
 		const user = interaction.options.getUser('user')
-			? interaction.options.getUser('user')
-			: interaction.user
-
 		let channelEmbed: string = interaction.guild.members.cache.get(user.id)
-			.voice.channel?.name
-
-		if (channelEmbed == null) channelEmbed = 'Not in the channel'
+			.voice.channel?.id
 
 		const Embed = new MessageEmbed()
 			.setColor(client.env.BOT_COLOR)
-			.setTitle(user.tag)
+			.setTitle(`${user.tag} ${user.bot ? '[bot]' : ''}`)
 			.setDescription(`Server member: ${interaction.guild.name}`)
 			.setThumbnail(user.displayAvatarURL({ dynamic: true }))
 			.addFields(
-				{ name: 'Channel', value: channelEmbed },
+				{ name: 'Channel', value: channelEmbed != null ? `<#${channelEmbed}>` : 'Not in channel'},
 				{ name: 'Id', value: user.id }
 			)
 			.setTimestamp()
