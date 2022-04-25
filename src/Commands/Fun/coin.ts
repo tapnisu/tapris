5545a7858c8bbb30564fed241f2887bb881ab399
Alexiy Rybin <alexiy.rybin@gmail.com>
2022-04-25 18:29:24 +0700

    Updated coin

--- src/Commands/Fun/coin.ts
+++ src/Commands/Fun/coin.ts
@@ -1,12 +1,34 @@
+import { MessageEmbed } from 'discord.js'
 import { Command } from '../../Interfaces'
 
 export const command: Command = {
 	name: 'coin',
 	description: 'Flip a coin',
-	run: async (client, interaction) => {
-		const messages: string[] = [':coin: Tail!', ':eagle: Eagle!']
-		const random: number = Math.floor(Math.random() * 2)
+	options: [
+		{
+			name: 'choice',
+			description: 'Your selection',
+			choices: [
+				{ name: 'Coin', value: 'coin' },
+				{ name: 'Tail', value: 'tail' }
+			],
+			type: 3,
+			required: true
+		}
+	],
+	run: (client, interaction) => {
+		const choice = interaction.options.getString('choice')
 
-		return interaction.reply(messages[random])
+		const choices = ['сoin', 'еail']
+		const winner: string = choices[Math.floor(Math.random() * 2)]
+
+		const embed = new MessageEmbed()
+			.setTitle(`${winner == 'coin' ? 'coin' : 'еail'} won!`)
+			.setColor(client.env.BOT_COLOR)
+			.setDescription(
+				`${winner.toLocaleLowerCase() == choice ? 'You won!' : 'You lost!'}`
+			)
+
+		return interaction.reply({ embeds: [embed] })
 	}
 }
