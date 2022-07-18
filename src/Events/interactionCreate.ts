0016d3ee9475c490400fc3f36e5016933edf3b4a
Alexiy Rybin <0xtapnisu@gmail.com>
2022-07-18 20:18:00 +0700

    Fixed interactions options errors

--- src/Events/interactionCreate.ts
+++ src/Events/interactionCreate.ts
@@ -1,22 +1,24 @@
 import { Button, Command, Event } from "../Interfaces";
 
-import { Interaction } from "discord.js";
+import { Interaction, InteractionType } from "discord.js";
 
 export const event: Event = {
 	name: "interactionCreate",
 	run: (client, interaction: Interaction) => {
-		if (interaction.isChatInputCommand()) {
+		if (interaction.type === InteractionType.ApplicationCommand) {
 			const command = client.commands.get(interaction.commandName);
 			if (command) (command as Command).run(client, interaction);
 
 			return;
 		}
 
-		if (interaction.isButton()) {
-			const button = client.buttons.find((button) =>
-				button.customId.test(interaction.customId)
-			);
-			if (button) (button as Button).run(client, interaction);
+		if (interaction.type === InteractionType.MessageComponent) {
+			if (interaction.isButton()) {
+				const button = client.buttons.find((button) =>
+					button.customId.test(interaction.customId)
+				);
+				if (button) (button as Button).run(client, interaction);
+			}
 		}
 	}
 };
