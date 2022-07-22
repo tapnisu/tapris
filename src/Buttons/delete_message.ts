45c43a3bd2d247002e035fcf2af3dab1ea16b6d6
Alexiy Rybin <0xtapnisu@gmail.com>
2022-07-22 17:56:59 +0700

    Removed variable from delete_message.ts

--- src/Buttons/delete_message.ts
+++ src/Buttons/delete_message.ts
@@ -1,11 +1,8 @@
 import { Button } from "../Interfaces";
-import { Message } from "discord.js";
 
 export const button: Button = {
 	customId: /delete_message/,
 	run: async (client, interaction) => {
-		const message = interaction.message as Message;
-
-		return message.delete();
+		return interaction.message.delete();
 	}
 };
