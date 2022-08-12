f366736a03c95e616368654d9dfbe07bcd16a709
Alexiy Rybin <0xtapnisu@gmail.com>
2022-08-12 13:58:17 +0700

    Translated invite button for guild create

--- src/Events/guildCreate.ts
+++ src/Events/guildCreate.ts
@@ -42,7 +42,7 @@ export const event: Event = {
 		const buttonsRow = new ActionRowBuilder<ButtonBuilder>().addComponents([
 			new ButtonBuilder()
 				.setURL(link)
-				.setLabel("Invite bot")
+				.setLabel(guildCreateLocale.invite())
 				.setStyle(ButtonStyle.Link)
 		]);
 
