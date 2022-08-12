aa6efa84a89c7476177a6f3f2b6f112e788cb68c
Alexiy Rybin <57483029+tapnisu@users.noreply.github.com>
2022-08-12 14:19:25 +0700

    Update from db branch (#269)
    * Added base system
    
    * Updated music to db
    
    * Added db support to gun
    
    * Bump version
    
    * Fixed on error shift

--- src/Commands/Music/skip.ts
+++ src/Commands/Music/skip.ts
@@ -12,7 +12,7 @@ export const command: Command = {
 	description: "Skip current music",
 	run: async (client, interaction) => {
 		await interaction.deferReply();
-
+    
 		const guild = await getGuild(interaction.guildId);
 
 		guild.queue.shift();
@@ -22,7 +22,7 @@ export const command: Command = {
 			return await interaction.followUp({
 				content: "The queue is empty now!"
 			});
-
+      
 		const connection = joinVoiceChannel({
 			channelId: (interaction.member as GuildMember).voice.channel.id,
 			guildId: interaction.guildId,
