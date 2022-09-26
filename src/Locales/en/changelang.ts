0a149efbc5ead9ae2b30d0ff16b418069aea0ffe
Alexiy Rybin <0xtapnisu@gmail.com>
2022-09-26 22:47:44 +0700

    Fixed change land en locale

--- src/Locales/en/changelang.ts
+++ src/Locales/en/changelang.ts
@@ -1,3 +1,3 @@
-export const success = (lang: string) => `Language i now set to ${lang}!`;
+export const success = (lang: string) => `Language is now set to ${lang}!`;
 export const notAdministrator = () =>
 	"You need to be an Administrator to change bots language!";
