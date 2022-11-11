1d3539287b7c0a0b22f65f205e945b614b69a3c4
Alexiy Rybin <0xtapnisu@gmail.com>
2022-11-11 18:54:49 +0700

    Fixed return type in Command type

--- src/Interfaces/Command.ts
+++ src/Interfaces/Command.ts
@@ -1,17 +1,25 @@
 import {
 	ApplicationCommandOption,
-	ChatInputCommandInteraction
+	ChatInputCommandInteraction,
+	InteractionResponse,
+	Message
 } from "discord.js";
 import Client from "../Core";
 
 interface Run {
-	(client: Client, interaction: ChatInputCommandInteraction): Promise<any>;
+	(client: Client, interaction: ChatInputCommandInteraction): Promise<
+		Message<boolean> | InteractionResponse<boolean>
+	>;
 }
+
+type ApplicationCommandOptionExtended = ApplicationCommandOption & {
+	required: boolean;
+};
 
 export interface Command {
 	name: string;
 	description?: string;
-	options?: ApplicationCommandOption[];
+	options?: ApplicationCommandOptionExtended[];
 	guildsOnly?: boolean;
 	run: Run;
 }
