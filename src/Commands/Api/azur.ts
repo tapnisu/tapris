c8844662e3c57ef7b3857ea6cf30246d1a93a48d
Alexiy Rybin <0xtapnisu@gmail.com>
2022-11-12 16:55:17 +0700

    Made azur better

--- src/Commands/Api/azur.ts
+++ src/Commands/Api/azur.ts
@@ -22,40 +22,38 @@ export const command: Command = {
 			interaction.options.getString("name").toLowerCase().replace(/ /g, "_")
 		);
 
-		let response: AzurResponse;
-
 		try {
-			response = (
+			const response: AzurResponse = (
 				await axios.get(
 					`https://raw.githubusercontent.com/alg-wiki/wikia/master/Ships/${request}.json`
 				)
 			).data;
+
+			await interaction.deferReply();
+
+			const Embed = new EmbedBuilder()
+				.setColor(client.env.BOT_COLOR)
+				.setTitle(response.name)
+				.setURL(`https://azurlane.koumakan.jp/wiki/${request}`)
+				.setDescription(response.rarity)
+				.addFields([
+					{ name: azurLocale.shipId, value: response.ID, inline: true },
+					{ name: azurLocale.shipHull, value: response.hull, inline: true },
+					{ name: azurLocale.shipNavy, value: response.navy, inline: true },
+					{ name: azurLocale.shipClass, value: response.class, inline: true },
+					{
+						name: azurLocale.shipVoiceActress,
+						value: response.voiceActress,
+						inline: true
+					}
+				]);
+
+			return await interaction.followUp({ embeds: [Embed] });
 		} catch {
 			return await interaction.reply({
 				content: azurLocale.shipNotFound,
 				ephemeral: true
 			});
 		}
-
-		await interaction.deferReply();
-
-		const Embed = new EmbedBuilder()
-			.setColor(client.env.BOT_COLOR)
-			.setTitle(response.name)
-			.setURL(`https://azurlane.koumakan.jp/wiki/${request}`)
-			.setDescription(response.rarity)
-			.addFields([
-				{ name: azurLocale.shipId, value: response.ID, inline: true },
-				{ name: azurLocale.shipHull, value: response.hull, inline: true },
-				{ name: azurLocale.shipNavy, value: response.navy, inline: true },
-				{ name: azurLocale.shipClass, value: response.class, inline: true },
-				{
-					name: azurLocale.shipVoiceActress,
-					value: response.voiceActress,
-					inline: true
-				}
-			]);
-
-		return await interaction.followUp({ embeds: [Embed] });
 	}
 };
