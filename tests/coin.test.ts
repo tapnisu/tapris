d4a12e5d844d95f5661c495857cfb2df39ae09bd
Alexiy Rybin <0xtapnisu@gmail.com>
2022-11-12 17:49:32 +0700

    fixed test name

--- tests/coin.test.ts
+++ tests/coin.test.ts
@@ -1,7 +1,7 @@
 import { describe, expect, test } from "@jest/globals";
 import { choices, flipCoin } from "../src/Exports/coin";
 
-describe("sum module", () => {
+describe("flipCoin function", () => {
 	test("Result of flipCoin be in choices", () => {
 		expect(choices).toContain(flipCoin(choices));
 	});
