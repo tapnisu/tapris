50e48ea726396d8e4c40760e69aa0193c6db8498
Alexiy Rybin <0xtapnisu@gmail.com>
2023-03-05 19:36:24 +0700

    Add delete commands

---
+++ src/deleteCommands.ts
@@ -0,0 +1,20 @@
+import { Client, REST, Routes } from "discord.js";
+import config from "./Core/env";
+
+const main = async () => {
+	const rest = new REST({ version: "10" }).setToken(config.token);
+	const client = new Client({
+		intents: [],
+	});
+	await client.login(config.token);
+
+	const commands = await rest.get(Routes.applicationCommands(client.user.id)) as unknown[];
+
+	commands.forEach((command: any) => {
+		rest.delete(Routes.applicationCommand(client.user.id, command.id))
+			.then(() => console.log(`Successfully deleted application command ${command.name}`))
+			.catch(console.error);
+	});
+};
+
+main();
