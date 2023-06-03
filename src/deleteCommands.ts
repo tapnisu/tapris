3692a60db99210cfbefabed3ac39e8140e1dbcfe
Alexiy Rybin <0xtapnisu@gmail.com>
2023-06-03 12:12:27 +0700

    Format

--- src/deleteCommands.ts
+++ src/deleteCommands.ts
@@ -4,15 +4,20 @@ import config from "./Core/env";
 const main = async () => {
 	const rest = new REST({ version: "10" }).setToken(config.token);
 	const client = new Client({
-		intents: [],
+		intents: []
 	});
 	await client.login(config.token);
 
-	const commands = await rest.get(Routes.applicationCommands(client.user.id)) as unknown[];
+	const commands = (await rest.get(
+		Routes.applicationCommands(client.user.id)
+	)) as unknown[];
 
 	commands.forEach((command: any) => {
-		rest.delete(Routes.applicationCommand(client.user.id, command.id))
-			.then(() => console.log(`Successfully deleted application command ${command.name}`))
+		rest
+			.delete(Routes.applicationCommand(client.user.id, command.id))
+			.then(() =>
+				console.log(`Successfully deleted application command ${command.name}`)
+			)
 			.catch(console.error);
 	});
 
