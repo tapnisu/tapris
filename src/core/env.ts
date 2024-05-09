d2554fd19f21b5496a1c5581df842589018d9869
Aleksei Rybin <0xalekseirybin@gmail.com>
2024-05-09 16:10:51 +0700

    BOT_COLOR fix

--- src/core/env.ts
+++ src/core/env.ts
@@ -1,7 +1,6 @@
 import dotenv from "dotenv";
 
 dotenv.config();
-
-process.env.BOT_TOKEN = process.env.BOT_TOKEN || "#abb1c2";
+process.env.BOT_COLOR = process.env.BOT_COLOR || "#abb1c2";
 
 export default process.env;
