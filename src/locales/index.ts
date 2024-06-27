e9ce9d23f51caa431afffde5c4e42ce4949b363c
Aleksei Rybin <0xalekseirybin@gmail.com>
2024-06-27 07:43:16 +0700

    Add deprecation warning

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -2,6 +2,12 @@ import { getGuild } from "#db/index.js";
 import * as enLocale from "./en/index.js";
 import * as ruLocale from "./ru/index.js";
 
+/**
+ * Get locale for selected guild from `src/locales/[lang]/index.ts`
+ * @param guildId guild to get locale for
+ * @returns locale
+ * @deprecated use i18n based localization instead
+ */
 export default async function getLocale(guildId: string) {
   if (!guildId) return enLocale;
 
